9ffc970fd3e7d457625ad9101b26a801b1f2e201
Vito <vitocrosa8@gmail.com>
2022-10-26 10:37:10 +0200

    produit inseré

--- front/js/product.js
+++ front/js/product.js
@@ -11,11 +11,116 @@ const fetchproduct = async () => {
         console.log(produitTab);
        
     })
-
-
 }
 
-fetchproduct()
+
+
+
+const produits = async () => {
+    await fetchproduct()
+
+
+
+     document.querySelector(".item__img").innerHTML = `
+      <img class = "item__img" src=${produitTab.imageUrl} alt="${produitTab.altTxt}">
+      `
+      document.querySelector(".item__content__titlePrice").innerHTML = `
+      <h1 id="title">${produitTab.name}</h1>
+      `
+
+      document.querySelector(".item__content__titlePrice").innerHTML = `
+      <p>Prix : <span id="price">${produitTab.price}</span>€</p>
+      `
+
+      document.querySelector(".item__content__description").innerHTML = `
+      <p id="description">${produitTab.description}</p>
+      `
+
+      let color = document.querySelector("#colors")
+     
+
+// rappel La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau
+      produitTab.colors.forEach((couleur) => {
+        
+        console.log(couleur);
+        let options = document.createElement("option")
+
+        options.innerHTML = `${couleur}`
+        options.value = `${couleur}`
+
+        color.appendChild(options)
+       
+
+
+      })
+
+   
+}
+
+
+produits()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
