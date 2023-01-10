b0e165384ecdc02d8522657469f4f306a5d89678
Vito <vitocrosa8@gmail.com>
2023-01-10 16:19:53 +0100

    propriété quantité

--- front/js/product.js
+++ front/js/product.js
@@ -47,27 +47,55 @@ const buttonPanier = document.getElementById("addToCart");
 buttonPanier.addEventListener("click", () => {
     const quantity = document.getElementById("quantity");
 
-    while (quantity.value <= 0) {
-        parseInt(quantity.value);
-    }
-
     const color = document.getElementById("colors");
 
-    let cart = JSON.parse(localStorage.getItem("cart"));
+    // si la couleur n'est pas égal a 0 donc couleur pas choisie alors ça ne marche pas et si la quantité n'est pas entre 100 et 0 alors ça ne marche pas
+    // J'utilise les ET logique pour rajouter des contraintes pour l'excution du produit si un est pas respecté alors ça ne s'applique pas
 
-    if (!cart) {
-        cart = [];
+    if (color.value != 0 && quantity.value <= 100 && quantity.value > 0) {
+        // si la carte existe deja alors on creer sinon ou creer pas on saute le if ça nous permet d'avoir un seul tableau avec tous les éléments
+        // et de rajouter a chaque ajout de canape dans la meme cart
+        let cart = JSON.parse(localStorage.getItem("cart"));
+
+        if (!cart) {
+            cart = [];
+        }
+        //  je vais chercher la valeur qui correspond a mes conditions grace a .find
+        let settingQuantity = cart.find(
+            (product) => product.id === id && product.color === color.value
+        );
+        // utilise le settingQuantity.quantity a la place de product.quantity car en déclarant la varible settingQuantity
+        // on a incrémenté product donc elle a pris ça valeur
+        if (settingQuantity) {
+            settingQuantity.quantity += parseInt(quantity.value);
+        } else {
+            const product = {
+                id: id,
+                color: color.value,
+                quantity: parseInt(quantity.value),
+            };
+            cart.push(product);
+        }
+
+        // console.log(settingQuantity);
+
+        console.log(cart);
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+    } else {
+        let erreur = document.createElement("h3");
+        erreur.innerText =
+            "Veuillez Choisir une couleur et une quantité de 1 a 100";
+        erreur.style.color = "black";
+
+        let parentsErreur = document.querySelector(
+            ".item__content__settings__quantity"
+        );
+        parentsErreur.appendChild(erreur);
+        buttonPanier.disabled = true;
+        setTimeout(() => {
+            erreur.innerText = "";
+            buttonPanier.disabled = false;
+        }, 2000);
     }
-
-    const product = {
-        id: id,
-        color: color.value,
-        quantity: quantity.value,
-    };
-
-    cart.push(product);
-
-    console.log(cart);
-
-    localStorage.setItem("cart", JSON.stringify(cart));
 });
